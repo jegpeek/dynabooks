c404f232786b632951d38c71d649e13675bd4e2f
Erik Tollerud <erik.tollerud@gmail.com>
2018-09-26 11:47:40 -0400

    make book_maker respect output dirs

--- book_maker.py
+++ book_maker.py
@@ -1,3 +1,4 @@
+import os
 import jinja2
 
 
@@ -9,4 +10,19 @@ def render_parameterized_notebook(templatefn, outfn, params):
     with open(outfn, 'w') as f:
             f.write(result)
 
-eriks_magic = render_parameterized_notebook
+
+def make_notebook_from_params(paramdct):
+    templatefn = paramdct.pop('template')
+    if not os.path.exists(templatefn):
+        templatefn = templatefn + '.ipynb'
+
+    basename = os.path.splitext(os.path.split(templatefn)[1])[0]
+    outnbfn = os.path.join('output_nbs', basename)
+
+    render_parameterized_notebook(templatefn, outnbfn, paramdct)
+
+    return outnbfn
+
+
+def generate_html_from_notebook(nbfn):
+    raise NotImplementedError
